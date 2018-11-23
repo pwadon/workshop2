1ff3f24681af4a0fce5c4d37e6fa513fe3b29713
Przemyslaw Wadon <p.wadon@gazeta.pl>
2018-11-23 15:15:06 +0100

    Workshop2

--- src/programing_school/UserProgram.java
+++ src/programing_school/UserProgram.java
@@ -12,90 +12,61 @@ import java.util.List;
 import java.util.Scanner;
 
 public class UserProgram {
-    public static void main(String[] args) {
+        public static void main(String[] args) {
 
-        choseWhatToDo();
+            choseWhatToDo();
 
-    }
-
-    private static void choseWhatToDo() {
-
-        Scanner scan = new Scanner(System.in);
-        System.out.println("podaj swoje id");
-
-        while(!scan.hasNextInt()){
+        }
+        private static void choseWhatToDo() {
+            Scanner scan = new Scanner(System.in);
             System.out.println("podaj swoje id");
-            scan.next();
-        }
-        int id = scan.nextInt();
-
-        System.out.println("\n" + "wybierz jedną z opcji: " + "\n" + " add - dodanie rozwiązania" + "\n"
-                + " view - przeglądanie swoich rozwiązań" + "\n" + " quit - zakończenie programu");
-        scanner:
-        while (true) {
-            System.out.println("wybierz co chcesz zrobić : ");
-            String operation = ScannerService.getString();
-            switch (operation) {
-                case "add":
-                    add();
-                    break;
-                case "view":
-                    view();
-                    break;
-                case "quit":
-                    break scanner;
+            while(!scan.hasNextInt()){
+                System.out.println("podaj swoje id");
+                scan.next();
+            }
+            int id = scan.nextInt();
+            System.out.println("\n" + "wybierz jedną z opcji: " + "\n" + " add - dodanie rozwiązania" + "\n"
+                    + " view - przeglądanie swoich rozwiązań" + "\n" + " quit - zakończenie programu");
+            scanner:
+            while (true) {
+                System.out.println("wybierz co chcesz zrobić : ");
+                String operation = ScannerService.getString();
+                switch (operation) {
+                    case "add":
+                        add(id);
+                        break;
+                    case "view":
+                        view(id);
+                        break;
+                    case "quit":
+                        break scanner;
+                }
             }
         }
-    }
-
-//    private static void choseWhatToDo() {
-//        System.out.println("\n" + "wybierz jedną z opcji: " + "\n" + " add - dodanie rozwiązania" + "\n"
-//                + " view - przeglądanie swoich rozwiązań" + "\n" + " quit - zakończenie programu");
-//        scanner:
-//        while (true) {
-//            System.out.println("wybierz co chcesz zrobić : ");
-//            String operation = ScannerService.getString();
-//            switch (operation) {
-//                case "add":
-//                    add();
-//                    break;
-//                case "view":
-//                    view();
-//                    break;
-//                case "quit":
-//                    break scanner;
-//            }
-//        }
-//    }
-
-    private static void add() {
-
-        List<solution> solutions = SolutionDao.allSolutions();
-        for (solution solution : solutions) {
-            if (solution.getDescription() == null) {
+        private static void add(int id) {
+            List<solution> solutionList = SolutionDao.loadALLByUserId(id);
+            for(solution solution : solutionList)
+                if (solution.getDescription() == null)
+                    System.out.println(solution);
+            System.out.println(("Podaj id zadania"));
+            int exercise_Id = ScannerService.getInt("podaj wartosc int");
+            solution solutioncheck = SolutionDao.SelectSolutionByUserAndExerciseId(id,exercise_Id);
+            if(solutioncheck.getDescription()==null)
+            {
+                System.out.println("Podaj rozwiązanie zadania");
+                String description = ScannerService.getString();
+                Date date = new Date();
+                solution solution = new solution();
+                solution.setExercise_id(exercise_Id);
+                solution.setDescription(description);
+                solution.setCreated(date.toLocaleString());
+                SolutionDao.update(solution);
+            }
+            else System.out.println("rozwiazanie juz zostalo dodane");
+        }
+        private static void view(int id) {
+            List<solution> solutionList = SolutionDao.loadALLByUserId(id);
+            for(solution solution : solutionList)
                 System.out.println(solution);
-            }
         }
-
-
-        System.out.println(("Podaj id zadania"));
-        int exercise_Id = ScannerService.getInt("podaj wartosc int");
-
-        System.out.println("Podaj rozwiązanie zadania");
-        String description =ScannerService.getString();
-
-        solution solution = new solution();
-        solution.setId(exercise_Id);
-        solution.setDescription(description);
-
-        SolutionDao.update(solution);
-
     }
-
-    private static void view() {
-        System.out.println("podaj id aby zobaczyc swoje rozwiazania");
-        int id = ScannerService.getInt("podaj poprawna wartosc liczbowa");
-
-        System.out.println(SolutionDao.SelectSolutionById(id));
-    }
-}
